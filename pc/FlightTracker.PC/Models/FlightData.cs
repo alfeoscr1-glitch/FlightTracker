45c1e15a7e89c0ea35628bd092685fab82033a2b
alfeoscr1-glitch <alfeoscr1@gmail.com>
2026-09-11 18:44:09 +0100

    Update FlightData.cs

--- pc/FlightTracker.PC/Models/FlightData.cs
+++ pc/FlightTracker.PC/Models/FlightData.cs
@@ -1,1 +1,20 @@
+namespace FlightTracker.PC.Models;
 
+public class FlightData
+{
+    public string Callsign { get; set; } = "";
+    public string AircraftType { get; set; } = "";
+    public string Registration { get; set; } = "";
+
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    public double Altitude { get; set; }
+    public double GroundSpeed { get; set; }
+    public double Heading { get; set; }
+    public double VerticalSpeed { get; set; }
+
+    public string Squawk { get; set; } = "";
+
+    public long Timestamp { get; set; }
+}
