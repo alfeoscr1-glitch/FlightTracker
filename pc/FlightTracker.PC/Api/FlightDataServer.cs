163117316694e353a7a7c3793f81ed076ee124d2
alfeoscr1-glitch <alfeoscr1@gmail.com>
2026-09-11 18:48:15 +0100

    Update FlightDataServer.cs

--- pc/FlightTracker.PC/Api/FlightDataServer.cs
+++ pc/FlightTracker.PC/Api/FlightDataServer.cs
@@ -1,1 +1,44 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using FlightTracker.PC.Models;
 
+namespace FlightTracker.PC.Api;
+
+public class FlightDataServer : IDisposable
+{
+    private readonly UdpClient _udpClient;
+    private readonly IPEndPoint _broadcastEndpoint;
+
+    public FlightDataServer(int udpPort = 49000)
+    {
+        _udpClient = new UdpClient
+        {
+            EnableBroadcast = true
+        };
+
+        _broadcastEndpoint = new IPEndPoint(
+            IPAddress.Broadcast,
+            udpPort
+        );
+    }
+
+    public async Task SendAsync(FlightData flightData)
+    {
+        string json = JsonSerializer.Serialize(flightData);
+
+        byte[] data = Encoding.UTF8.GetBytes(json);
+
+        await _udpClient.SendAsync(
+            data,
+            data.Length,
+            _broadcastEndpoint
+        );
+    }
+
+    public void Dispose()
+    {
+        _udpClient.Dispose();
+    }
+}
